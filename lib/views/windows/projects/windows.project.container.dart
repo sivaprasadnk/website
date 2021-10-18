8f014246fc9eb9622a481ca8d3612b0ab0453f36
Mac <mac@Macs-MacBook-Pro.local>
2021-10-18 15:43:29 +0530

    revamping projects ui

--- lib/views/windows/projects/windows.project.container.dart
+++ lib/views/windows/projects/windows.project.container.dart
@@ -1,5 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:page_view_indicators/page_view_indicators.dart';
 import 'package:spnk/utils/common_strings.dart';
 import 'package:spnk/utils/common_widgets.dart';
 
@@ -35,11 +36,166 @@ class MyPainter extends CustomPainter {
   bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
 
-class WindowsProjectContainer extends StatelessWidget {
+class WindowsProjectContainer extends StatefulWidget {
   final double screenWidth;
   final double screenHeight;
   const WindowsProjectContainer(
       {required this.screenWidth, required this.screenHeight});
+
+  @override
+  _WindowsProjectContainerState createState() =>
+      _WindowsProjectContainerState();
+}
+
+class _WindowsProjectContainerState extends State<WindowsProjectContainer> {
+  final _pageController2 = PageController();
+  final _currentPageNotifier2 = ValueNotifier<int>(0);
+
+  int i = 0;
+
+  _buildPageView(
+          PageController pageController, ValueNotifier currentPageNotifier) =>
+      Container(
+        width: widget.screenWidth * 0.9,
+        height: widget.screenHeight * 0.6,
+        child: PageView.builder(
+            itemCount: 2,
+            controller: pageController,
+            itemBuilder: (BuildContext context, int index) {
+              return index == 0
+                  ? Padding(
+                      padding: EdgeInsets.only(
+                        left: widget.screenWidth * 0.2,
+                        top: widget.screenHeight * 0.1,
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: widget.screenWidth * 0.15,
+                            child: Image(
+                              height: 250,
+                              image: AssetImage(
+                                'assets/images/quizApp/screen2.png',
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: widget.screenHeight * 0.1,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20),
+                                child: Container(
+                                  width: widget.screenWidth * 0.15,
+                                  child: FittedBox(
+                                    child: Text(
+                                      "SP Quiz App",
+                                      style: TextStyle(
+                                        // fontFamily: 'PlayfairDisplay',
+                                        fontWeight: FontWeight.bold,
+                                        // fontSize: 40,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20),
+                                child: Container(
+                                  // width: screenWidth * 0.15,
+                                  child: Text(
+                                    "A Simple quiz app \nwith cool animations and effects",
+                                    style: TextStyle(
+                                      // fontFamily: 'PlayfairDisplay',
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 30),
+                              GooglePlayButton(
+                                  screenWidth: widget.screenWidth,
+                                  url: spQuizLink),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  : Padding(
+                      padding: EdgeInsets.only(
+                        left: widget.screenWidth * 0.2,
+                        top: widget.screenHeight * 0.1,
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: widget.screenWidth * 0.15,
+                            child: Image(
+                              height: 250,
+                              image: AssetImage(
+                                'assets/images/quizApp/screen2.png',
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: widget.screenHeight * 0.1,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20),
+                                child: Container(
+                                  width: widget.screenWidth * 0.15,
+                                  child: FittedBox(
+                                    child: Text(
+                                      "SP Quotes App",
+                                      style: TextStyle(
+                                        // fontFamily: 'PlayfairDisplay',
+                                        fontWeight: FontWeight.bold,
+                                        // fontSize: 40,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20),
+                                child: Container(
+                                  // width: screenWidth * 0.15,
+                                  child: Text(
+                                    "A Simple Quotes listing app",
+                                    style: TextStyle(
+                                      // fontFamily: 'PlayfairDisplay',
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 30),
+                              GooglePlayButton(
+                                  screenWidth: widget.screenWidth,
+                                  url: spQuotesLink),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+            },
+            onPageChanged: (int index) {
+              currentPageNotifier.value = index;
+            }),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -51,13 +207,13 @@ class WindowsProjectContainer extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                height: screenHeight * 0.1,
+                height: widget.screenHeight * 0.1,
               ),
               Padding(
-                padding: EdgeInsets.only(left: screenWidth * 0.2),
+                padding: EdgeInsets.only(left: widget.screenWidth * 0.2),
                 child: FadeInDownBig(
                   child: Container(
-                    width: screenWidth * 0.2,
+                    width: widget.screenWidth * 0.2,
                     child: FittedBox(
                       child: Text(
                         "My Projects",
@@ -73,82 +229,47 @@ class WindowsProjectContainer extends StatelessWidget {
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(
-                  left: screenWidth * 0.2,
-                  top: screenHeight * 0.1,
-                ),
-                child: FadeInLeftBig(
-                  child: Container(
-                    width: screenWidth * 0.15,
-                    child: FittedBox(
-                      child: Text(
-                        "SP Quiz App",
-                        style: TextStyle(
-                          // fontFamily: 'PlayfairDisplay',
-                          fontWeight: FontWeight.bold,
-                          // fontSize: 40,
-                          color: Colors.white,
+                padding: EdgeInsets.only(left: widget.screenWidth * 0.05),
+                child: ArrowPageIndicator(
+                  iconPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                  isInside: true,
+                  leftIcon: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                  rightIcon: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Center(
+                        child: Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          size: 30,
                         ),
                       ),
                     ),
                   ),
+                  pageController: _pageController2,
+                  currentPageNotifier: _currentPageNotifier2,
+                  itemCount: 2,
+                  child:
+                      _buildPageView(_pageController2, _currentPageNotifier2),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  left: screenWidth * 0.2,
-                  top: screenHeight * 0.1,
-                ),
-                child: FadeInLeftBig(
-                  child: Row(
-                    children: [
-                      Container(
-                        width: screenWidth * 0.15,
-                        child: Image(
-                          height: 250,
-                          image: AssetImage(
-                            'assets/images/quizApp/screen2.png',
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20),
-                            child: Container(
-                              // width: screenWidth * 0.15,
-                              child: Text(
-                                "A Simple quiz app \n with cool animations and effects",
-                                style: TextStyle(
-                                  // fontFamily: 'PlayfairDisplay',
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 30),
-                          // Container(
-                          //   width: screenWidth * 0.15,
-                          //   child: Image(
-                          //     height: 150,
-                          //     image: AssetImage(
-                          //       'assets/images/quizApp/screen2.png',
-                          //     ),
-                          //   ),
-                          // ),
-                          GooglePlayButton(
-                              screenWidth: screenWidth, url: spQuizLink),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              )
             ],
-          )
+          ),
         ],
       ),
     );
