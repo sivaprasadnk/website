f90ccc47ea5c137e137e9eac98570b7e177a4a3c
sivaprasadnk <sivaprasadnk123@gmail.com>
2024-06-05 22:02:24 +0530

    project navigation correction

--- lib/views/windows/large/projects/projects.screen.new.dart
+++ lib/views/windows/large/projects/projects.screen.new.dart
@@ -20,103 +20,116 @@ class ProjectsScreenNew extends StatelessWidget {
   ///
   @override
   Widget build(BuildContext context) {
-    // final pageIndex = ref.watch(pageIndexProvider);
     final projects = dataController.projects;
-    final pageIndex = pageIndexController.pageIndex.value;
 
-    final showNextIcon = pageIndex < projects.length - 1;
-    final showPrevIcon = pageIndex != 0;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: context.screenHeight * 0.2),
-        const WindowsLargeSectionTitle(
-          title: 'My Projects',
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        SizedBox(
-          width: context.screenWidth * 0.9,
-          height: context.screenHeight * 0.5,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SizedBox(
-                width: 150,
-              ),
-              if (showPrevIcon)
-                GestureDetector(
-                  onTap: () {
-                    controller.previousPage(
-                      duration: const Duration(seconds: 1),
-                      curve: Curves.bounceOut,
-                    );
-                  },
-                  child: SizedBox(
-                    height: 20,
-                    width: 20,
-                    child: Icon(
-                      Icons.arrow_back_ios,
-                      color: Theme.of(context).splashColor,
-                    ).showCursorOnHover,
+    return GetX<PageIndexController>(
+      builder: (pageIndexController) {
+        final pageIndex = pageIndexController.pageIndex.value;
+
+        final showNextIcon = pageIndex < projects.length - 1;
+        final showPrevIcon = pageIndex != 0;
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: context.screenHeight * 0.2),
+            const WindowsLargeSectionTitle(
+              title: 'My Projects',
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              width: context.screenWidth * 0.9,
+              height: context.screenHeight * 0.5,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(
+                    width: 150,
                   ),
-                ),
-              SizedBox(
-                height: context.screenHeight * 0.5,
-                width: 800,
-                child: PageView(
-                  onPageChanged: (page) {
-                    // ref.read(pageIndexProvider.notifier).pageIndex =
-                    //     page.toDouble();
-                  },
-                  controller: controller,
-                  children: projects.map((project) {
-                    return SizedBox(
-                      height: context.screenHeight * 0.5,
-                      child: Row(
-                        children: [
-                          ImageContainer(
-                            imagePath: project.bgAssetPath,
-                            isWeb: project.isWeb,
-                          ),
-                          DetailsContainer(
-                            desc1: project.descList.first,
-                            desc2: project.descList.last,
-                            tools1: project.techStackList.first,
-                            tools2: project.techStackList.last,
-                            link: project.url,
-                            title: project.projName,
-                            isWeb: project.isWeb,
-                          ),
-                        ],
+                  if (showPrevIcon)
+                    GestureDetector(
+                      onTap: () {
+                        controller.previousPage(
+                          duration: const Duration(seconds: 1),
+                          curve: Curves.bounceOut,
+                        );
+                      },
+                      child: SizedBox(
+                        height: 20,
+                        width: 20,
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          color: Theme.of(context).splashColor,
+                        ).showCursorOnHover,
                       ),
-                    );
-                  }).toList(),
-                ),
-              ),
-              if (showNextIcon)
-                GestureDetector(
-                  onTap: () {
-                    controller.nextPage(
-                      duration: const Duration(seconds: 1),
-                      curve: Curves.bounceOut,
-                    );
-                  },
-                  child: SizedBox(
-                    height: 20,
-                    width: 20,
-                    child: Icon(
-                      Icons.arrow_forward_ios,
-                      color: Theme.of(context).splashColor,
-                    ).showCursorOnHover,
+                    )
+                  else
+                    const SizedBox(
+                      height: 20,
+                      width: 20,
+                    ),
+                  SizedBox(
+                    height: context.screenHeight * 0.5,
+                    width: 800,
+                    child: PageView(
+                      onPageChanged: (page) {
+                        pageIndexController.pageIndex.value = page;
+                      },
+                      controller: controller,
+                      children: projects.map((project) {
+                        return SizedBox(
+                          height: context.screenHeight * 0.5,
+                          child: Row(
+                            children: [
+                              ImageContainer(
+                                imagePath: project.bgAssetPath,
+                                isWeb: project.isWeb,
+                              ),
+                              DetailsContainer(
+                                desc1: project.descList.first,
+                                desc2: project.descList.last,
+                                tools1: project.techStackList.first,
+                                tools2: project.techStackList.last,
+                                link: project.url,
+                                title: project.projName,
+                                isWeb: project.isWeb,
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    ),
                   ),
-                ),
-            ],
-          ),
-            
-        ),
-      ],
+                  if (showNextIcon)
+                    GestureDetector(
+                      onTap: () {
+                        controller.nextPage(
+                          duration: const Duration(seconds: 1),
+                          curve: Curves.bounceOut,
+                        );
+                        pageIndexController.pageIndex.value += 1;
+                      },
+                      child: SizedBox(
+                        height: 20,
+                        width: 20,
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Theme.of(context).splashColor,
+                        ).showCursorOnHover,
+                      ),
+                    )
+                  else
+                    const SizedBox(
+                      height: 20,
+                      width: 20,
+                    ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
